0b248d1371c5da39a92645f414906d1f319802cf
Garry Ledwith <garryledwith@gmail.com>
2025-07-16 05:12:25 +0100

    for loops and recursion for fibonacci numbers

--- fibonacci.py
+++ fibonacci.py
@@ -3,6 +3,9 @@
 # fib(1) = 1
 # fib(n) = fib(n-1) + fib(n-2) for n > 1
 
+# Fibonacci formula: 
+# F(n) = F(n-1) + F(n-2)
+
 # fibonacci algorithm: 
 # get the first two fibonacci numbers
 # then use a loop to calculate the next numbers up to n by adding the last two numbers
@@ -30,4 +33,21 @@ for fibo in range(2, 19):
     # update the last two Fibonacci numbers
     prev1 = prev2
     prev2 = new_fibo
+    
+# 2. implementation of Fibonacci numbers using recursion
+last1 = 0
+last2 = 1
+count = 2 # begin counting from the third Fibonacci number
+
+def fibonacci_recursive(last1, last2):
+    global count
+    if count <= 19:
+        new_fibo = last1 + last2
+        print(new_fibo)
+        count += 1
+        fibonacci_recursive(last2, new_fibo) # recursive call to continue the sequence
+    else: 
+        return
+    
+fibonacci_recursive(last1, last2) # call the recursive function to start the sequence
     
